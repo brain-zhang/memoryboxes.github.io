022e744371f4661f24d778fbe824ce81d395cc5c
memorybox <memoryboxes@gmail.com>
2018-07-21 12:42:00 +0800

    migrate vm instances and snapshots from different vps supporters or accounts

--- publish.sh
+++ publish.sh
@@ -1,4 +1,7 @@
 #/bin/bash
+
+# image url(put images folders of branch source):
+# [!背景](https://github.com/memoryboxes/memoryboxes.github.io/blob/source/images/201612/bg1.jpg)
 bundle exec rake generate
 echo "happy123.me" > public/CNAME
 bundle exec rake deploy
