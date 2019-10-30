0ede1e46366e1d883a6c57159454362ac64bc25b
brain-zhang <brain.zhangbin@outlook.com>
2019-10-30 10:13:25 +0800

    How to run tmux service scripts on ubuntu start up

--- publish.sh
+++ publish.sh
@@ -2,6 +2,7 @@
 
 # image url(put images folders of branch source):
 # ![背景](https://github.com/brain-zhang/memoryboxes.github.io/blob/source/images/201612/bg1.jpg)
+#cd _deploy && git pull && cd ../
 bundle exec rake generate
 echo "happy123.me" > public/CNAME
 bundle exec rake deploy
